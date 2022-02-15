338490635a07361859de256669a3ed15467f282c
RafaelXokito <82586689+RafaelXokito@users.noreply.github.com>
2022-02-15 18:51:18 +0000

    Initial Exceptions

---
+++ src/main/java/pt/ipleiria/estg/dei/ei/UpFeed/exceptions/MyUnauthorizedException.java
@@ -0,0 +1,7 @@
+package pt.ipleiria.estg.dei.ei.UpFeed.exceptions;
+
+public class MyUnauthorizedException extends Exception{
+    public MyUnauthorizedException(String message) {
+        super(message);
+    }
+}
